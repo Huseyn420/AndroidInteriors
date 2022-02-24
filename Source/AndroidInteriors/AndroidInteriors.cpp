8e7c8b309216a2c19debac595eeb603abc1d1679
Huseyn420 <huseyn20@gmail.com>
2022-02-24 19:30:32 +0300

    Apartment interior

---
+++ Source/AndroidInteriors/AndroidInteriors.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AndroidInteriors.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AndroidInteriors, "AndroidInteriors" );
---
+++ Source/AndroidInteriors/AndroidInteriorsGameModeBase.h
@@ -0,0 +1,18 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AndroidInteriorsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANDROIDINTERIORS_API AAndroidInteriorsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+	AAndroidInteriorsGameModeBase();
+};
